b0fde21f5aef8e18b38eb0aa673389e6f92d2f6c
Никита Васильняк <nvasil94@bk.ru>
2024-02-05 23:22:39 +0300

    Вставляем картинку и изменили code.js

--- code.js
+++ code.js
@@ -1,2 +1,10 @@
-console.log('Hello');
-console.log('HelloWorld');
+//console.log('Hello');
+//console.log('HelloWorld');
+
+import Person from './Person';
+
+const person1 = new Person('Nikita', 'Vasilnyak');
+const person2 = new Person('Yra', 'Kiselev');
+
+console.log(person1);
+console.log(person2);
